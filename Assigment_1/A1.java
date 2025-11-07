b99073de37caf8dbac3110f973d48ae37a1d5abf
Hugo Sjöholm <111504314+HugoSjoholm@users.noreply.github.com>
2025-11-07 14:02:30 +0100

    once again not working

--- Assigment_1/A1.java
+++ Assigment_1/A1.java
@@ -9,34 +9,34 @@ public class A1{
 		int totalHours = Integer.parseInt(args[0]);
 		double dailyEffort = Double.parseDouble(args[1]);
 
-		double timeToComplete = totalHours / dailyEffort;
+		double amountOfDays = totalHours / dailyEffort;
 		
 		// Modify and complete the following code
+		
+		int years = (int)((amountOfDays / 365));
+		int months = (int)((amountOfDays % 365)/30);
+		int days = (int)((amountOfDays % 365)%30);
+		int hours = (int)((amountOfDays % 365)%(30*24));
+		// amountOfDays = 0;
 
-		int days = (int)(timeToComplete);
-		int months = (int)((timeToComplete)/MONTH);
-		int years = (int)((timeToComplete/(YEAR*HOURS_PER_DAY)));
-		int hours = totalHours;
-		//timeToComplete = 0;
-
-		int minutes = 0;
+		int minutes = (int)((amountOfDays % 365)%(30*24*60));
 
 
 		// WARNING! Do not modify these outputs. Use these exact strings.
-		System.out.println("timeToComplete " + timeToComplete);
+		System.out.println("amountOfDays " + amountOfDays);
 		System.out.println("Hours to completion: " + totalHours);
 		System.out.println("Daily dedication: " + dailyEffort);
 		System.out.print("it will take you: ");
 
-		if (years > 0)
+		if (true)
 			System.out.print(years + " years, ");
-		if (months > 0)
+		if (true)
 			System.out.print(months + " months, ");
-		if (days > 0)
+		if (true)
 			System.out.print(days + " days, ");
-		if (hours > 0)
+		if (true)
 			System.out.print(hours + " hours, ");
-		if (minutes > 0)
+		if (true)
 			System.out.print(minutes + " minutes, ");
 
 		System.out.println();
---
+++ Assigment_1/test.java
@@ -0,0 +1,7 @@
+public class A1{
+
+	public static void main(String[] args) {
+        int year = 8760;
+        System.out.print((9840 % year)/30.0/24.0);
+   }
+}
