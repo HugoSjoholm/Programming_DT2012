313dee050f354c5b8c3e823e0b0f781e0039d7bf
Hugo Sjöholm <111504314+HugoSjoholm@users.noreply.github.com>
2025-11-07 15:41:23 +0100

    Added debug var

--- Assigment_1/A1.java
+++ Assigment_1/A1.java
@@ -4,46 +4,59 @@ public class A1 {
 	private static final int MONTH = 30;
 	private static final int HOURS_PER_DAY = 24;
 	private static final int MINUTES_PER_HOUR = 60;
+	// added by student
+	private static boolean overaide = false; // declaring overraid varidable used for debugging, cus I can't make this
+												// crap work.
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_RED = "\u001B[31m";
 
 	public static void main(String[] args) {
+		if (Integer.parseInt(args[2]) == 1) {
+			overaide = true;
+			System.out.println(ANSI_RED + "overaide activated" + ANSI_RESET);
+		}
+
 		int totalHours = Integer.parseInt(args[0]);
 		double dailyEffort = Double.parseDouble(args[1]);
 
 		double amountOfDays = totalHours / dailyEffort;
+		if (overaide) {
+			amountOfDays = Integer.parseInt(args[0]);
+		}
 
 		// Modify and complete the following code
 
 		int years = (int) ((amountOfDays / 365));
-		double yearRemainder = (amountOfDays % 365);
+		int yearRemainder = (int) (amountOfDays % 365);
 
 		int months = (int) (yearRemainder / 30);
-		double monthRemainder = (yearRemainder % 30);
+		int monthRemainder = (int) (yearRemainder % 30);
 
 		int days = (int) (monthRemainder / 24);
-		double dayRemainder = (monthRemainder % 24);
+		int dayRemainder = (int) (monthRemainder % 24);
 
 		int hours = (int) (dayRemainder / 60);
-		double hoursRemainder = (dayRemainder % 60);
+		int hoursRemainder = (int) (dayRemainder % 60);
 		// amountOfDays = 0;
 
 		int minutes = (int) hoursRemainder;
 
 		// WARNING! Do not modify these outputs. Use these exact strings.
-		System.out.println("amountOfDays " + amountOfDays);
-		System.out.println("Hours to completion: " + totalHours);
-		System.out.println("Daily dedication: " + dailyEffort);
+		System.out.print("amountOfDays " + amountOfDays);
+		System.out.print(" - Hours to completion: " + totalHours);
+		System.out.println(" - Daily dedication: " + dailyEffort);
 		System.out.print("it will take you: ");
 
 		if (true)
-			System.out.print(years + " years, ");
+			System.out.print(years + " years " + "(" + yearRemainder + "), ");
 		if (true)
-			System.out.print(months + " months, ");
+			System.out.print(months + " months " + "(" + monthRemainder + "), ");
 		if (true)
-			System.out.print(days + " days, ");
+			System.out.print(days + " days " + "(" + dayRemainder + "), ");
 		if (true)
-			System.out.print(hours + " hours, ");
+			System.out.print(hours + " hours " + "(" + hoursRemainder + "), ");
 		if (true)
-			System.out.print(minutes + " minutes, ");
+			System.out.print(minutes + " minutes ");
 
 		System.out.println();
 	}
