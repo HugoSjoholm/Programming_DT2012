4282a3b964d9d1a9809578614eea61348f34eda8
Hugo Sjöholm <111504314+HugoSjoholm@users.noreply.github.com>
2025-11-14 22:42:55 +0100

    Moved file structure
    Also added week 3_4 and A1 for week3_4

---
+++ Week_3_4/A1.java
@@ -0,0 +1,28 @@
+public class A1 {
+	
+	public static void main(String[] args) {
+		int numDataPoints = Integer.parseInt(args[0]);
+		String dateOfBirth = "";
+		int phoneNumber = 0;
+		int salary = 0;
+
+		int minYear = 0;
+		int maxYear = ;
+		int maxSalary = 39000;
+		int minSalary = 28800;
+
+		// Print the number of data points
+		System.out.println(numDataPoints);
+		
+		// Generate and print all the data points
+		// For each datapoint:
+			// Print the birthdate and a space
+
+			// On the same line, print the telephone number and a space
+
+			// On the same line, print the random salary in SEK without öre and break the line
+
+		
+
+	}
+}
