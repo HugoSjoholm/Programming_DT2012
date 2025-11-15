36c46f931763a3814b845d8a0b8b6990511fad68
Hugo Sjöholm <111504314+HugoSjoholm@users.noreply.github.com>
2025-11-15 04:09:18 +0100

    A1 Finished
    A1 now returns an output matching the problem description. It's noteworthy that it does not follow the return spec that was in the base copy A1 (as it was given out from the teacher)

--- Week_3_4/A1.java
+++ Week_3_4/A1.java
@@ -1,5 +1,5 @@
 public class A1 {
-	
+
 	public static void main(String[] args) {
 		int numDataPoints = Integer.parseInt(args[0]);
 		String dateOfBirth = "";
@@ -7,22 +7,63 @@ public class A1 {
 		int salary = 0;
 
 		int minYear = 0;
-		int maxYear = ;
+		int maxYear = 0;
 		int maxSalary = 39000;
 		int minSalary = 28800;
 
+		//fun stuff. ignore this if you're grading
+		boolean commaSeperated = false;
+		try {
+			commaSeperated = args[1].equals("c");
+		}
+		catch (Exception e) {
+			//System.out.println("normal seperator");
+		}
+
+		System.out.println("Number of data points " + numDataPoints);
+		for (int i = 0; i < numDataPoints; i++) {
+			// age
+				// years
+				int age_year = (int) ((Math.random() * 48) + 18 + 1960);
+
+				// month
+				int[] daysInMonth = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+				int age_month = (int) (Math.random() * 12 + 1);
+				age_month = 2;
+				// day
+				int leapDay = 0;
+				if ((age_year % 4 == 0 && age_year % 100 != 0) || (age_year % 400 == 0)) { //checks if it's a leap year with some math
+					leapDay = 1;
+				}
+				int age_day = (int) (Math.random() * daysInMonth[age_month-1] + 1 + leapDay);
+			
+			//telefone
+				String phone = "07";
+				phone = phone + (int)(Math.random() * Math.pow(10, 8));
+
+			//salary
+				int salaryButLocal = (int)(((Math.random() * (maxSalary - minSalary)) + minSalary)*100);
+				double salaryRounded = (double)(salaryButLocal/100.0);
+
+			if (commaSeperated) {
+				System.out.println(age_year + "-" + age_month + "-" + age_day + "," + phone + "," + salaryRounded);
+			}
+			else {
+				System.out.println(age_year + "-" + age_month + "-" + age_day + " " + phone + " " + salaryRounded);
+			}
+		}
+
 		// Print the number of data points
-		System.out.println(numDataPoints);
-		
+		// System.out.println(numDataPoints);
+
 		// Generate and print all the data points
 		// For each datapoint:
-			// Print the birthdate and a space
+		// Print the birthdate and a space
 
-			// On the same line, print the telephone number and a space
+		// On the same line, print the telephone number and a space
 
-			// On the same line, print the random salary in SEK without öre and break the line
-
-		
+		// On the same line, print the random salary in SEK without öre and break the
+		// line
 
 	}
 }
