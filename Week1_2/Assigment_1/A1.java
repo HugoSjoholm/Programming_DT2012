25bb94f317b5085db90cdac3a798d6ca2d06962d
Hugo Sjöholm <111504314+HugoSjoholm@users.noreply.github.com>
2025-11-16 16:48:50 +0100

    A1 cleaned to fit spec
    removed all the fun and intresting part so it now fits the assigment specification

--- Week1_2/Assigment_1/A1.java
+++ Week1_2/Assigment_1/A1.java
@@ -4,30 +4,15 @@ public class A1 {
 	private static final int MONTH = 30;
 	private static final int HOURS_PER_DAY = 24;
 	private static final int MINUTES_PER_HOUR = 60;
-	// added by student
-	private static boolean overaide = false; // declaring overraid varidable used for debugging
-	public static final String ANSI_RESET = "\u001B[0m";
-	public static final String ANSI_RED = "\u001B[31m";
-	public static final String ANSI_GREEN = "\u001B[32m";
 
 	public static void main(String[] args) {
-		try { // ignore this, debug mode
-			if (Integer.parseInt(args[2]) == 1) {
-				overaide = true;
-				System.out.println(ANSI_RED + "overaide activated" + ANSI_RESET);
-			}
-		} catch (Exception e) { // do nothing
-		}
-
 		int totalHours = Integer.parseInt(args[0]);
 		double dailyEffort = Double.parseDouble(args[1]);
 
-		double amountOfDays = totalHours / dailyEffort; // Changed variable name to ammouf of days to better represent
-														// the value stored
-		if (overaide) {
-			amountOfDays = Integer.parseInt(args[0]);
-		}
+		double timeToComplete = totalHours / dailyEffort;
+
 		// Modify and complete the following code
+		double amountOfDays = timeToComplete; // Changed variable name to ammouf of days to better represent the value stored
 
 		int years = (int) ((amountOfDays / 365));
 		double yearRemainder = (amountOfDays % 365);
@@ -48,32 +33,18 @@ public class A1 {
 		System.out.print("amountOfDays = " + amountOfDays);
 		System.out.print(" - Hours to completion: " + totalHours);
 		System.out.println(" - Daily dedication: " + dailyEffort);
-		if (overaide) {
-			if (true)
-				System.out.print(ANSI_GREEN + years + " years " + "+ " + yearRemainder + " days, ");
-			if (true)
-				System.out.print(months + " months " + "+ " + monthRemainder + " days, ");
-			if (true)
-				System.out.print(days + " days " + "(" + dayRemainder + "), ");
-			if (true)
-				System.out.print(hours + " hours " + "(" + hoursRemainder + "), ");
-			if (true)
-				System.out.println(minutes + " minutes ");
-		} else {
-			System.out.print("it will take you: ");
 
-			if (years > 0)
-				System.out.print(ANSI_GREEN + years + " years, ");
-			if (months > 0)
-				System.out.print(months + " months, ");
-			if (days > 0)
-				System.out.print(days + " days, ");
-			if (hours > 0)
-				System.out.print(hours + " hours, ");
-			if (minutes > 0)
-				System.out.println(minutes + " minutes ");
-		}
+		if (true)
+			System.out.print(years + " years " + "+ " + yearRemainder + " days, ");
+		if (true)
+			System.out.print(months + " months " + "+ " + monthRemainder + " days, ");
+		if (true)
+			System.out.print(days + " days " + "(" + dayRemainder + "), ");
+		if (true)
+			System.out.print(hours + " hours " + "(" + hoursRemainder + "), ");
+		if (true)
+			System.out.println(minutes + " minutes ");
 
-		System.out.println(ANSI_RESET + " - ");
+		System.out.println(" - ");
 	}
 }
