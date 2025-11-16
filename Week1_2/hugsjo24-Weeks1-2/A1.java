9df3ff9bf3dcd6a68ef275c78e7eee25572fb2f6
Hugo Sjöholm <111504314+HugoSjoholm@users.noreply.github.com>
2025-11-16 17:14:43 +0100

    Added subbission zip

---
+++ Week1_2/hugsjo24-Weeks1-2/A1.java
@@ -0,0 +1,50 @@
+public class A1 {
+
+	private static final int YEAR = 365;
+	private static final int MONTH = 30;
+	private static final int HOURS_PER_DAY = 24;
+	private static final int MINUTES_PER_HOUR = 60;
+
+	public static void main(String[] args) {
+		int totalHours = Integer.parseInt(args[0]);
+		double dailyEffort = Double.parseDouble(args[1]);
+
+		double timeToComplete = totalHours / dailyEffort;
+
+		// Modify and complete the following code
+		double amountOfDays = timeToComplete; // Changed variable name to ammouf of days to better represent the value stored
+
+		int years = (int) ((amountOfDays / 365));
+		double yearRemainder = (amountOfDays % 365);
+
+		int months = (int) (yearRemainder / 30);
+		double monthRemainder = (yearRemainder % 30);
+
+		int days = (int) (monthRemainder);
+		double dayRemainder = (monthRemainder - days);
+
+		int hours = (int) (dayRemainder * 24);
+		double hoursRemainder = ((dayRemainder * 24) - hours);
+		// amountOfDays = 0;
+
+		int minutes = (int) (hoursRemainder * 60);
+
+		// WARNING! Do not modify these outputs. Use these exact strings.
+		System.out.print("amountOfDays = " + amountOfDays);
+		System.out.print(" - Hours to completion: " + totalHours);
+		System.out.println(" - Daily dedication: " + dailyEffort);
+
+		if (true)
+			System.out.print(years + " years " + "+ " + yearRemainder + " days, ");
+		if (true)
+			System.out.print(months + " months " + "+ " + monthRemainder + " days, ");
+		if (true)
+			System.out.print(days + " days " + "(" + dayRemainder + "), ");
+		if (true)
+			System.out.print(hours + " hours " + "(" + hoursRemainder + "), ");
+		if (true)
+			System.out.println(minutes + " minutes ");
+
+		System.out.println(" - ");
+	}
+}
