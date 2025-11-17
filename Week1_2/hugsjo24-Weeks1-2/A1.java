d35df08fddc17f3255b62bc7e2b05476502aaec9
Hugo Sjöholm <111504314+HugoSjoholm@users.noreply.github.com>
2025-11-17 20:10:43 +0100

    COMPLETLEY cleaned up w1-2 A1

--- Week1_2/hugsjo24-Weeks1-2/A1.java
+++ Week1_2/hugsjo24-Weeks1-2/A1.java
@@ -30,21 +30,22 @@ public class A1 {
 		int minutes = (int) (hoursRemainder * 60);
 
 		// WARNING! Do not modify these outputs. Use these exact strings.
-		System.out.print("amountOfDays = " + amountOfDays);
-		System.out.print(" - Hours to completion: " + totalHours);
-		System.out.println(" - Daily dedication: " + dailyEffort);
+		//System.out.print("amountOfDays = " + amountOfDays);
+		System.out.println("Hours to completion: " + totalHours);
+		System.out.println("Daily dedication: " + dailyEffort);
+		System.out.print("it will take you: ");
 
-		if (true)
-			System.out.print(years + " years " + "+ " + yearRemainder + " days, ");
-		if (true)
-			System.out.print(months + " months " + "+ " + monthRemainder + " days, ");
-		if (true)
-			System.out.print(days + " days " + "(" + dayRemainder + "), ");
-		if (true)
-			System.out.print(hours + " hours " + "(" + hoursRemainder + "), ");
-		if (true)
-			System.out.println(minutes + " minutes ");
+		if (years > 0)
+			System.out.print(years + " years, ");
+		if (months > 0)
+			System.out.print(months + " months, ");
+		if (days > 0)
+			System.out.print(days + " days, ");
+		if (hours > 0)
+			System.out.print(hours + " hours, ");
+		if (minutes > 0)
+			System.out.print(minutes + " minutes, ");
 
-		System.out.println(" - ");
+		System.out.println();
 	}
 }
