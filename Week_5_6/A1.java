58ea5ce0804ca96bc9e581e41849edb98f7d4103
Hugo Sjöholm <111504314+HugoSjoholm@users.noreply.github.com>
2025-12-03 19:46:52 +0100

    A1 complete

--- Week_5_6/A1.java
+++ Week_5_6/A1.java
@@ -17,13 +17,13 @@ public class A1 {
         }
       }
         newPic.show();
-
+        newPic.save("pic2.jpg");
         // Implement the complement method
         // Remember to do to the file what is expected (show, save)
     }
     public static void blackAndWhite(Picture pic) {
       Picture newPic = new Picture(pic);
-      System.out.println("Make it gray scale");
+      //System.out.println("Make it gray scale");
       for (int x = 0; x < newPic.width(); x++) {
         for (int y = 0; y < newPic.height(); y++) {
           Color tmp = newPic.get(x, y);
@@ -39,12 +39,29 @@ public class A1 {
     }
 
     public static void rotate(Picture pic) {
-      
+      Picture newPicR = new Picture(pic);
+      for (int x = 0; x < newPicR.width(); x++) {
+        for (int y = 0; y < newPicR.height(); y++) {
+          newPicR.set(pic.height() - 1 - y, x, pic.get(x,y));
+        }
+      }
+
+      newPicR.show();
+      newPicR.save("pic3.jpg");
       // Implement the rotate method
       // Remember to do to the file what is expected (show, save)
     }
     
     public static void flip(Picture pic) {
+      Picture newPic = new Picture(pic);
+      for (int x = 0; x < pic.width(); x++) {
+        for (int y = 0; y < pic.width(); y++) {
+          Color tmp = pic.get(x, y);
+          newPic.set(pic.width() - 1 - x, y, tmp);
+        }
+      }
+      newPic.show();
+      newPic.save("pic4.jpg");
       // Implement the flip method
       // Remember to do to the file what is expected (show, save)
     }
@@ -52,9 +69,9 @@ public class A1 {
     public static void main(String[] args) {
       String filename = args[0];
       Picture input = new Picture(filename);
-      System.out.println(filename);
-      StdDraw.setScale(0,input.width());
-      StdDraw.picture(input.width()/2,input.height()/2,filename);
+      //System.out.println(filename);
+      //StdDraw.setScale(0,input.width());
+      //StdDraw.picture(input.width()/2,input.height()/2,filename);
       //StdDraw.show();
 
 
@@ -62,6 +79,8 @@ public class A1 {
         // Test EACH and ALL of the methods 
         blackAndWhite(input);
         complement(input);
+        rotate(input);
+        flip(input);
         
         //StdDraw.save("pic1.jpg");
 
